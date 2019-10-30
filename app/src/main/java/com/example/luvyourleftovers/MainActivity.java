8a39e3b3406cbb188c886d0da9aa1e2865813d6d
Lorenzo Battilocchi <Lorenzo.battilocchi@ucdconnect.ie>
2019-10-30 12:57:48 +0000

    Import libraries for design and set custom icons in app package
    WHAT
    - set custom icons
    - import UX Design libraries
    
    WHY
    
    To make the app nicer

---
+++ app/src/main/java/com/example/luvyourleftovers/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.luvyourleftovers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// TODO:
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
