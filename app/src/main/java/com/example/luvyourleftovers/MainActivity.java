532ee1fcd3457cc26fa1ad987a4ec2f37a9f1aae
Lorenzo Battilocchi (AS208114) <Lorenzo.battilocchi@ucdconnect.ie>
2019-11-10 23:54:52 +0000

    First implementation of Recipe List + update .gitignore
    **WHAT**
    - Made sample RecyclerView List with action listener (for now only prints out "You clicked X item" but this can be extended to do whatever we need. 
    - Separated out layout files (1 for row items of the RecyclerView, one for the actual RecyclerView)
    
    **WHY**
    - Makes it more efficient in terms of memory usage than rendering a plain ListView
    - If we decide the layout of the items needs to change, we can do that quickly by editing just one file

--- app/src/main/java/com/example/luvyourleftovers/MainActivity.java
+++ app/src/main/java/com/example/luvyourleftovers/MainActivity.java
@@ -1,16 +1,54 @@
 package com.example.luvyourleftovers;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-// TODO:
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements RecyclerViewAdapter.ItemClickListener {
+
+    RecyclerViewAdapter rvaAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // data to populate the RecyclerView with
+        ArrayList<String> recipeHeaders = new ArrayList<>();
+
+        //Add each of the recipe headers to the ArrayList
+        recipeHeaders.add("Pikachu Fried");
+        recipeHeaders.add("Chicken");
+        recipeHeaders.add("Yogurt");
+        recipeHeaders.add("Creme Brule");
+        recipeHeaders.add("Profiterol");
+        recipeHeaders.add("Pasta");
+        recipeHeaders.add("Samosa");
+        recipeHeaders.add("Couscous");
+        recipeHeaders.add("Sandwich");
+        recipeHeaders.add("Burger");
+        recipeHeaders.add("Lasagna");
+        recipeHeaders.add("Cheesecake");
+        recipeHeaders.add("Chocolate Mousse");
+        recipeHeaders.add("Soup");
+
+        // set up the RecyclerView
+        RecyclerView recyclerView = findViewById(R.id.rvRecipes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        rvaAdapter = new RecyclerViewAdapter(this, recipeHeaders);
+        rvaAdapter.setClickListener(this);
+        recyclerView.setAdapter(rvaAdapter);
+    }
+
+    @Override
+    public void onItemClick(View view, int position) {
+        //Sending toast message, but it can also call a method to execute any intent/function call available in-app
+        Toast.makeText(this, "You clicked " + rvaAdapter.getItem(position) + " on row number " + (position+1), Toast.LENGTH_SHORT).show();
     }
 }
