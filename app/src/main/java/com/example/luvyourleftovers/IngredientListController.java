8936b4437d130058efbcc18f63cdf5a25cc3c940
Royal Thomas <royal.thomas@ucdconnect.ie>
2019-11-20 14:05:23 +0000

    Added an input area and now sends the list of ingredients to a method to parse results

---
+++ app/src/main/java/com/example/luvyourleftovers/IngredientListController.java
@@ -0,0 +1,10 @@
+package com.example.luvyourleftovers;
+
+import android.content.Context;
+
+public class IngredientListController {
+    IngredientListController(Context context){
+
+    }
+
+}
--- app/src/main/java/com/example/luvyourleftovers/MainActivity.java
+++ app/src/main/java/com/example/luvyourleftovers/MainActivity.java
@@ -2,11 +2,15 @@ package com.example.luvyourleftovers;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import org.apmem.tools.layouts.FlowLayout;
 
 import java.util.ArrayList;
 
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
 
         // data to populate the RecyclerView with
         ArrayList<String> recipeHeaders = new ArrayList<>();
+        final ArrayList<String> ingredients = new ArrayList<>();
 
         //TODO: Make this pull dynamically from API (@Royal Thomas)
         //Add each of the recipe headers to the ArrayList
@@ -45,12 +50,55 @@ public class MainActivity extends AppCompatActivity implements RecyclerViewAdapt
         rvaAdapter = new RecyclerViewAdapter(this, recipeHeaders);
         rvaAdapter.setClickListener(this);
         recyclerView.setAdapter(rvaAdapter);
+
+        // set it up to get user inputs
+        final EditText ingredientInputArea = findViewById(R.id.inputBox);
+        Button searchButton = findViewById(R.id.searchButton);
+
+        // User inserts an ingredient. 
+        findViewById(R.id.insertButton).setOnClickListener((view) -> {
+            String input = ingredientInputArea.getText().toString();
+            if (!input.isEmpty()) {
+                addToContainer(ingredientInputArea.getText().toString(), ingredients);
+                ingredientInputArea.setText("");
+            }
+        });
+
+        // What happens when search button is clicked.
+        searchButton.setOnClickListener((view) -> {
+            performSearch(ingredients);
+        });
+
+    }
+
+    /**
+     * To be implemented, it just joins the arraylist using ,
+     */
+    public void performSearch(ArrayList<String> input) {
+        String formattedInput = android.text.TextUtils.join(",", input);
+        Toast.makeText(this, formattedInput, Toast.LENGTH_SHORT).show();
+    }
+
+
+    public void addToContainer(String text, final ArrayList<String> ingredients) {
+        ingredients.add(text);
+        Button newIngredientButton = new Button(this);
+        newIngredientButton.setText(text);
+        final FlowLayout flowLayout = findViewById(R.id.flowLayout);
+        flowLayout.addView(newIngredientButton);
+
+        newIngredientButton.setOnClickListener((v) -> {
+            ingredients.remove(newIngredientButton.getText());
+            flowLayout.removeView(v);
+        });
+
+        Toast.makeText(this, ingredients.toString(), Toast.LENGTH_SHORT).show();
     }
 
     //TODO: Grab data on link in API Response header (@Royal Thomas is this your part?)
     @Override
     public void onItemClick(View view, int position) {
         //Sending toast message, but it can also call a method to execute any intent/function call available in-app
-        Toast.makeText(this, "You clicked " + rvaAdapter.getItem(position) + " on row number " + (position+1), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "You clicked " + rvaAdapter.getItem(position) + " on row number " + (position + 1), Toast.LENGTH_SHORT).show();
     }
 }
