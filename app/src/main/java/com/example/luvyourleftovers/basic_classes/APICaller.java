725bcaa224c8f5b0086fa74358a67d83a54223ef
Bill-IN <will2@outlook.ie>
2019-11-26 19:08:47 +0000

    Added constructor to APICaller

--- app/src/main/java/com/example/luvyourleftovers/basic_classes/APICaller.java
+++ app/src/main/java/com/example/luvyourleftovers/basic_classes/APICaller.java
@@ -2,9 +2,13 @@ package com.example.luvyourleftovers.basic_classes;
 
 
 public class APICaller {
-    String endpoint = "https://api.spoonacular.com/"; //spoonacular API Endpoint
-    String recipesNode = "recipes/"; //node within the spoonacular API endpoint
-    String queryConstructor = "search?query=***LIST OF INGREDIENTS FROM @royal10march 's part***";
-    String ignorePantryIngredients = "&ignorePantry=true"; //this just tells the API to ignore absence of standard pantry ingredients
-    String apiKey = "&apiKey=***REPLACE WITH YOUR API KEY***"; //your personal API Key (OAuth Request)
+    private String endpoint = "https://api.spoonacular.com/"; //spoonacular API Endpoint
+    private String recipesNode = "recipes/"; //node within the spoonacular API endpoint
+    private String queryConstructor = "search?query=***LIST OF INGREDIENTS FROM @royal10march 's part***";
+    private String ignorePantryIngredients = "&ignorePantry=true"; //this just tells the API to ignore absence of standard pantry ingredients
+    private String apiKey = "&apiKey=***REPLACE WITH YOUR API KEY***"; //your personal API Key (OAuth Request)
+
+    public APICaller(String recipesNode, String queryConstructor, String ignorePantryIngredients, String apiKey){
+
+    }
 }
