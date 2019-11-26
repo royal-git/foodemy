f17b8fabb6e5f3cb1a396f6f99a79b9bf24f5329
Bill-IN <will2@outlook.ie>
2019-11-26 18:11:20 +0000

    Added initial fields

--- app/src/main/java/com/example/luvyourleftovers/basic_classes/APICaller.java
+++ app/src/main/java/com/example/luvyourleftovers/basic_classes/APICaller.java
@@ -1,5 +1,10 @@
 package com.example.luvyourleftovers.basic_classes;
 
+
 public class APICaller {
-    
+    String endpoint = "https://api.spoonacular.com/"; //spoonacular API Endpoint
+    String recipesNode = "recipes/"; //node within the spoonacular API endpoint
+    String queryConstructor = "search?query=***LIST OF INGREDIENTS FROM @royal10march 's part***";
+    String ignorePantryIngredients = "&ignorePantry=true"; //this just tells the API to ignore absence of standard pantry ingredients
+    String apiKey = "&apiKey=***REPLACE WITH YOUR API KEY***"; //your personal API Key (OAuth Request)
 }
