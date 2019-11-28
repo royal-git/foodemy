2db5e90ab3d40b53f3db1381e1fb5a6276938b9f
Lorenzo Battilocchi (AS208114) <Lorenzo.battilocchi@ucdconnect.ie>
2019-11-28 16:26:15 +0000

    Fix to Share Recipe Button
    Now working with default sharing options provided by Android

--- app/src/main/java/com/example/luvyourleftovers/DisplayRecipe.java
+++ app/src/main/java/com/example/luvyourleftovers/DisplayRecipe.java
@@ -2,7 +2,10 @@ package com.example.luvyourleftovers;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Activity displays the Recipe Selected by a User.
@@ -15,5 +18,15 @@ public class DisplayRecipe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_recipe);
+
+        Button shareButton = findViewById(R.id.share_button);
+        shareButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                sharingIntent.setType("text/plain");
+                startActivity(Intent.createChooser(sharingIntent, "Share"));
+            }
+        });
     }
 }
