7a96667b805823616b8c979e2b0596bcd2d1cc0a
Lorenzo Battilocchi (AS208114) <Lorenzo.battilocchi@ucdconnect.ie>
2019-11-28 14:04:02 +0000

    Minor fixes
    - Update implementations to 17.0.0
    - Fixed some access specifiers
    - Fixed some imports
    - Extracted Strings
    - Corrected some error toast messages
    - renamed class MainActivity (my one) into IngredientsRecipeActivity

--- app/src/main/java/com/example/luvyourleftovers/MainActivity.java
+++ app/src/main/java/com/example/luvyourleftovers/MainActivity.java
@@ -1,260 +1,90 @@
 package com.example.luvyourleftovers;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import com.example.luvyourleftovers.shopping_cart.CartDBHelper;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.ml.vision.FirebaseVision;
-import com.google.firebase.ml.vision.common.FirebaseVisionImage;
-import com.google.firebase.ml.vision.label.FirebaseVisionImageLabel;
-import com.google.firebase.ml.vision.label.FirebaseVisionImageLabeler;
-import com.koushikdutta.async.future.Future;
-import com.koushikdutta.ion.Ion;
-import java.util.ArrayList;
-import java.util.List;
-import org.apmem.tools.layouts.FlowLayout;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity implements
-    RecyclerViewAdapter.ItemClickListener {
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-  private static final int CAMERA_REQUEST = 1888;
-  private ImageView imageView;
-  private static final int MY_CAMERA_PERMISSION_CODE = 100;
-  RecyclerViewAdapter rvaAdapter;
-  ArrayList<String> ingredients;
-  Button searchButton;
-  CartDBHelper db;
-  FirebaseVisionImageLabeler detector;
+import androidx.appcompat.widget.Toolbar;
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-    db = new CartDBHelper(this);
+import com.google.android.material.navigation.NavigationView;
 
-    // data to populate the RecyclerView with
-    ArrayList<String> recipeHeaders = new ArrayList<>();
-    ingredients = new ArrayList<>();
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
+{
+    private DrawerLayout drawer;
 
-    //TODO: Make this pull dynamically from API (@Royal Thomas)
-    //Add each of the recipe headers to the ArrayList
-    recipeHeaders.add("Pikachu Fried");
-    recipeHeaders.add("Chicken");
-    recipeHeaders.add("Yogurt");
-    recipeHeaders.add("Creme Brule");
-    recipeHeaders.add("Profiterol");
-    recipeHeaders.add("Pasta");
-    recipeHeaders.add("Samosa");
-    recipeHeaders.add("Couscous");
-    recipeHeaders.add("Sandwich");
-    recipeHeaders.add("Burger");
-    recipeHeaders.add("Lasagna");
-    recipeHeaders.add("Cheesecake");
-    recipeHeaders.add("Chocolate Mousse");
-    recipeHeaders.add("Soup");
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
 
-    // set up the RecyclerView
-    RecyclerView recyclerView = findViewById(R.id.rvRecipes);
-    recyclerView.setLayoutManager(new LinearLayoutManager(this));
-    rvaAdapter = new RecyclerViewAdapter(this, recipeHeaders);
-    rvaAdapter.setClickListener(this);
-    recyclerView.setAdapter(rvaAdapter);
+        // Setting the tool bar per activity rather than using one for all of them for more flexibility.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-    // set it up to get user inputs
-    final EditText ingredientInputArea = findViewById(R.id.inputBox);
-    searchButton = findViewById(R.id.searchButton);
-    Button photoButton = findViewById(R.id.insertPhoto);
-    photoButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-          requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
-        } else {
-          Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-          startActivityForResult(cameraIntent, CAMERA_REQUEST);
+        drawer = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        // Automatically adds the button in the toolbar to open the navigation drawer.
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Setting the default tab/fragment on first open, avoiding the opening of the fragment each time there's an interruption.
+        if(savedInstanceState == null)
+        {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InputFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
         }
-      }
-    });
 
-    // User inserts an ingredient.
-    findViewById(R.id.insertButton).setOnClickListener((view) -> {
-      String input = ingredientInputArea.getText().toString();
-      if (!input.isEmpty()) {
-        addToContainer(ingredientInputArea.getText().toString());
-        ingredientInputArea.setText("");
-      }
-    });
-
-    // What happens when search button is clicked.
-    searchButton.setOnClickListener((view) -> {
-      // TODO add the ingredient to list of previously searched ingredients.
-      performSearch();
-    });
-
-  }
-
-  @Override
-  public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-      @NonNull int[] grantResults) {
-    super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-    if (requestCode == MY_CAMERA_PERMISSION_CODE) {
-      if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-        Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
-        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST);
-      } else {
-        Toast.makeText(this, "camera permission denied", Toast.LENGTH_LONG).show();
-      }
-    }
-  }
-
-
-  @Override
-  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    super.onActivityResult(requestCode, resultCode, data);
-    if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
-      Bitmap photo = (Bitmap) data.getExtras().get("data");
-      FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(photo);
-      detector = FirebaseVision.getInstance()
-          .getCloudImageLabeler();
-      callDetector(image);
-    }
-  }
-
-
-  public void performSearch() {
-    String formattedInput = android.text.TextUtils.join(",", ingredients);
-    Toast.makeText(this, formattedInput, Toast.LENGTH_SHORT).show();
-  }
-
-
-  public void addToContainer(String text) {
-    ingredients.add(text);
-    searchButton.setVisibility(View.VISIBLE);
-    Button newIngredientButton = new Button(this);
-    newIngredientButton.setText(text);
-    final FlowLayout flowLayout = findViewById(R.id.flowLayout);
-    flowLayout.addView(newIngredientButton);
-
-    newIngredientButton.setOnClickListener((v) -> {
-      ingredients.remove(newIngredientButton.getText());
-
-      flowLayout.removeView(v);
-      if (ingredients.size() == 0) {
-        searchButton.setVisibility(View.GONE);
-      }
-    });
-
-    Toast.makeText(this, ingredients.toString(), Toast.LENGTH_SHORT).show();
-  }
-
-  //TODO: Grab data on link in API Response header (@Royal Thomas is this your part?)
-  @Override
-  public void onItemClick(View view, int position) {
-    //Sending toast message, but it can also call a method to execute any intent/function call available in-app
-    Toast.makeText(this,
-        "You clicked " + rvaAdapter.getItem(position) + " on row number " + (position + 1),
-        Toast.LENGTH_SHORT).show();
-  }
-
-
-  protected void callDetector(FirebaseVisionImage image) {
-    ArrayList<String> results = new ArrayList<>();
-    Toast.makeText(this, "Generating Result, Hold on!", Toast.LENGTH_SHORT).show();
-
-    Task<List<FirebaseVisionImageLabel>> result =
-        detector.processImage(image)
-            .addOnSuccessListener(
-                labels -> {
-                  handleImageLebels(labels);
-                })
-            .addOnFailureListener(
-                new OnFailureListener() {
-                  @Override
-                  public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(MainActivity.this, "Error Fetching from Vision API.",
-                        Toast.LENGTH_SHORT).show();
-                  }
-                });
-
-  }
-
-  public void handleImageLebels(List<FirebaseVisionImageLabel> labels) {
-    ArrayList<String> results = new ArrayList<>();
-
-    for (FirebaseVisionImageLabel label : labels) {
-      String text = label.getText();
-      if (isRecognizedIngredient(text)) {
-        results.add(text);
-      }
     }
 
-    if (results.size() > 0) {
-      AlertDialog.Builder builder = new AlertDialog.Builder(this);
-      ArrayList<String> selections = new ArrayList<>();
-      builder.setTitle("Find any items?")
-          .setPositiveButton("Add to list!", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-              selections.forEach((value) -> {
-                addToContainer(value);
-              });
-            }
-          })
-          .setCancelable(false)
-          .setNegativeButton("Cancel", null)
-          .setMultiChoiceItems(results.toArray(new String[0]), null,
-              new DialogInterface.OnMultiChoiceClickListener() {
-                @Override
-                public void onClick(DialogInterface dialog, int which,
-                    boolean isChecked) {
-                  selections.add(results.get(which));
-                }
-              });
-      builder.show();
-    } else {
-      Toast.makeText(this, "Didn't find any ingredients, oops!",
-          Toast.LENGTH_SHORT).show();
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+    {
+        switch (menuItem.getItemId())
+        {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new InputFragment()).commit();
+                break;
+
+            case R.id.nav_favourites:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FavouritesFragment()).commit();
+                break;
+
+            case R.id.nav_shoppingList:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ShoppingListFragment()).commit();
+                break;
+
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
-  }
 
-  Boolean isRecognizedIngredient(String ingredient) {
-    Boolean validIngredient = false;
-    Future<String> result = Ion.with(this)
-        .load("http://royalthomas.me/checkIngredient.php?ingredient=" + uriEncode(ingredient))
-        .asString();
-    try {
-      if (result.get().contains("True")) {
-        validIngredient = true;
-      }
-
-    } catch (Exception ex) {
-      Log.d("MainActivity", "Ran into issue with checking if ingredient valid");
+    @Override
+    public void onBackPressed()
+    {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
+            drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
-    return validIngredient;
-  }
-
-  String uriEncode(String input) {
-    return input.replaceAll(" ", "+").toLowerCase();
-  }
-
-
 }
