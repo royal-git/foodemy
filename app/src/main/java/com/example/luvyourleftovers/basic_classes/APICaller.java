8689e4d87a2df2a0e75b9397f37916c2ed64def8
Royal Thomas <royal.thomas@ucdconnect.ie>
2019-11-29 23:13:48 +0000

    Said goodbye to a few fallen soldiers

--- app/src/main/java/com/example/luvyourleftovers/basic_classes/APICaller.java
+++ app/src/main/java/com/example/luvyourleftovers/basic_classes/APICaller.java
@@ -14,7 +14,6 @@ import okhttp3.Request;
 import okhttp3.Request.Builder;
 import okhttp3.Response;
 import xdroid.toaster.Toaster;
-//import xdroid.toaster.Toaster;
 
 /**
  * Class to call the Spoonacular API, Fetch Results and Build Objects based n the results.
