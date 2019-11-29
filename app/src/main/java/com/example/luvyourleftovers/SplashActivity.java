c9f53c2e46f9551759f251d78fc266379debfcea
Royal Thomas <royal.thomas@ucdconnect.ie>
2019-11-29 23:56:46 +0000

    Second Splash

--- app/src/main/java/com/example/luvyourleftovers/SplashActivity.java
+++ app/src/main/java/com/example/luvyourleftovers/SplashActivity.java
@@ -2,6 +2,7 @@ package com.example.luvyourleftovers;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 
 /**
@@ -13,19 +14,11 @@ public class SplashActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_splash);
-//        ImageView imageView = findViewById(R.id.imageView);
-//        imageView.setVisibility(View.VISIBLE);
-    try {
-      Thread.sleep(2000);
-      launchMain();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
 
   }
 
   // Simple button on the splash screen that launches the main activity.
-  public void launchMain() {
+  public void launchMain(View view) {
     Intent main = new Intent(this, IngredientsRecipesActivity.class);
     startActivity(main);
   }
